bd253542f4798abae9562e7e95b455e58c7b84fe
kushan <pckushan@gmail.com>
2019-10-19 16:48:22 +0530

    :tada: initial commit

---
+++ src/main/java/lk/code/limpiar/LimpiarApplication.java
@@ -0,0 +1,13 @@
+package lk.code.limpiar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimpiarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimpiarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/code/limpiar/LimpiarApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.code.limpiar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimpiarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
